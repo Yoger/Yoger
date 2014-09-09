dab91c33fdc7d1b50e571e2592595b68c71468f2
aq2004723 <onlyatale@gmail.com>
2014-09-09 09:36:38 +0800

    9-9

--- src/com/example/community/MainPageAct.java
+++ src/com/example/community/MainPageAct.java
@@ -12,6 +12,7 @@ import android.app.AlertDialog;
 import android.app.LocalActivityManager;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
@@ -22,6 +23,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
 @SuppressWarnings("deprecation")
 public class MainPageAct extends ActivityGroup {
@@ -33,6 +35,7 @@ public class MainPageAct extends ActivityGroup {
 	private ImageView dinnerImageView =null;
 	private ImageView sweetImageView =null;
 	private ImageView addImageView=null;
+	private TextView firstTv ,secondTv ,thirdTv ,fourthTv;
 
 
 	private LocalActivityManager localActivityManager = null;
@@ -66,16 +69,31 @@ public class MainPageAct extends ActivityGroup {
 
 		
 		breImageView = (ImageView) findViewById(R.id.bre);
+		firstTv=(TextView)findViewById(R.id.mainpage_first);
+		
+		lunchImageView = (ImageView) findViewById(R.id.lunch);
+		secondTv=(TextView)findViewById(R.id.mainpage_second);
+		
+		dinnerImageView = (ImageView)findViewById(R.id.dinner);
+		thirdTv = (TextView)findViewById(R.id.mainpage_third);
+		
+		sweetImageView = (ImageView)findViewById(R.id.sweet);
+		fourthTv = (TextView)findViewById(R.id.mainpage_fourth);
+		
+		
 		breImageView.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
+//				breImageView.setBackgroundResource(getResources().getIdentifier("home_s", "drawable", getPackageName()));
+				changeMainPageState(1);
 				messagesBox.removeAllViews();
 				messagesBox.addView(localActivityManager.startActivity("Module1",
 						new Intent(MainPageAct.this, HomePageAct.class).addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)).getDecorView());
 			}
 		});
-		lunchImageView = (ImageView) findViewById(R.id.lunch);
+		
 		lunchImageView.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
+				changeMainPageState(2);
 				messagesBox.removeAllViews();
 				messagesBox.addView(localActivityManager.startActivity("Module2",
 						new Intent(MainPageAct.this, MakeMoneyAct.class).addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)).getDecorView());
@@ -84,6 +102,7 @@ public class MainPageAct extends ActivityGroup {
 		dinnerImageView = (ImageView) findViewById(R.id.dinner);
 		dinnerImageView.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
+				changeMainPageState(3);
 				messagesBox.removeAllViews();
 				messagesBox.addView(localActivityManager.startActivity("Module3",
 						new Intent(MainPageAct.this, ClassifyAct.class).addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)).getDecorView());
@@ -92,6 +111,7 @@ public class MainPageAct extends ActivityGroup {
 		sweetImageView = (ImageView) findViewById(R.id.sweet);
 		sweetImageView.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
+				changeMainPageState(4);
 				messagesBox.removeAllViews();
 				messagesBox.addView(localActivityManager.startActivity("Module4",
 						new Intent(MainPageAct.this,InfoDetailActivity.class).addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)).getDecorView());
@@ -153,6 +173,53 @@ public class MainPageAct extends ActivityGroup {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_page, menu);
 		return true;
+	}
+	
+	public void changeMainPageState(int choose){
+		switch (choose) {
+		case 1:
+			breImageView.setImageResource(getResources().getIdentifier("home_s", "drawable", getPackageName()));
+			firstTv.setTextColor(Color.rgb(255, 80, 1));
+			lunchImageView.setImageResource(getResources().getIdentifier("money_ns","drawable", getPackageName()));
+			secondTv.setTextColor(Color.rgb(0, 0, 0));
+			dinnerImageView.setImageResource(getResources().getIdentifier("classify_ns","drawable", getPackageName()));
+			thirdTv.setTextColor(Color.rgb(0, 0, 0));
+			sweetImageView.setImageResource(getResources().getIdentifier("inf_ns","drawable", getPackageName()));
+			fourthTv.setTextColor(Color.rgb(0, 0, 0));			
+			break;
+		case 2:
+			breImageView.setImageResource(getResources().getIdentifier("home_ns", "drawable", getPackageName()));
+			firstTv.setTextColor(Color.rgb(0, 0, 0));
+			lunchImageView.setImageResource(getResources().getIdentifier("money_s","drawable", getPackageName()));
+			secondTv.setTextColor(Color.rgb(255,80,1));
+			dinnerImageView.setImageResource(getResources().getIdentifier("classify_ns","drawable", getPackageName()));
+			thirdTv.setTextColor(Color.rgb(0, 0, 0));
+			sweetImageView.setImageResource(getResources().getIdentifier("inf_ns","drawable", getPackageName()));
+			fourthTv.setTextColor(Color.rgb(0, 0, 0));			
+			break;
+		case 3:
+			breImageView.setImageResource(getResources().getIdentifier("home_ns", "drawable", getPackageName()));
+			firstTv.setTextColor(Color.rgb(0, 0, 0));
+			lunchImageView.setImageResource(getResources().getIdentifier("money_ns","drawable", getPackageName()));
+			secondTv.setTextColor(Color.rgb(0,0,0));
+			dinnerImageView.setImageResource(getResources().getIdentifier("classify_s","drawable", getPackageName()));
+			thirdTv.setTextColor(Color.rgb(255,80,1));
+			sweetImageView.setImageResource(getResources().getIdentifier("inf_ns","drawable", getPackageName()));
+			fourthTv.setTextColor(Color.rgb(0, 0, 0));			
+			break;
+		case 4:
+			breImageView.setImageResource(getResources().getIdentifier("home_ns", "drawable", getPackageName()));
+			firstTv.setTextColor(Color.rgb(0, 0, 0));
+			lunchImageView.setImageResource(getResources().getIdentifier("money_ns","drawable", getPackageName()));
+			secondTv.setTextColor(Color.rgb(0 , 0, 0));
+			dinnerImageView.setImageResource(getResources().getIdentifier("classify_ns","drawable", getPackageName()));
+			thirdTv.setTextColor(Color.rgb(0, 0, 0));
+			sweetImageView.setImageResource(getResources().getIdentifier("inf_s","drawable", getPackageName()));
+			fourthTv.setTextColor(Color.rgb(255, 80, 1));			
+			break;
+		default:
+			break;
+		}
 	}
 
 
